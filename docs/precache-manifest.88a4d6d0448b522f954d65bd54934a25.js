07ed88a6c15b83b3bbbc33c4fa70e37b8cd5c006
Juan Carlos Sierra <carlos.sierratrujillo@gmail.com>
2020-10-20 20:50:00 -0500

    docs agregado

---
+++ docs/precache-manifest.88a4d6d0448b522f954d65bd54934a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7bcbbd9561a9708ea66215e4100e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac04cee0547506f64b7",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac04cee0547506f64b7",
+    "url": "/static/js/main.c5c3162a.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
